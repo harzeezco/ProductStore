434d926d9a2ee4af1e09dda0939f59415adead31
TSRlearn <wakilazeez01@gmail.com>
2024-01-12 19:45:57 +0100

    refactor code...

--- client/components/layouts/footer/footer.tsx
+++ client/components/layouts/footer/footer.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import Container from '../../elements/container';
 import { FooterNavLinks } from '@/client/lib/placeholders';
 import NextLink from '../../elements/next-link';
 import { Input } from '../../forms/input';
 
 export function Footer() {
-  const image = 'url(/png/shared-assets/footer-illustration.png)';
+  const backgroundImage =
+    'url(/png/shared-assets/footer-illustration.png)';
 
   return (
     <footer className='overflow-hidden'>
       <div
-        className='footer-top flex gap-10 p-20 items-center'
+        className='footer-top bg-contain relative bg-no-repeat bg-bottom flex flex-col md:flex-row gap-10 p-20 md:items-center'
         style={{
-          backgroundImage: image,
+          backgroundImage,
           backgroundSize: 'cover',
+          borderBlockEnd: '2px solid hsla(245, 17%, 29%, 0.3)',
         }}
       >
         <div className='flex-[1.5] relative z-50'>
@@ -21,14 +22,17 @@ export function Footer() {
           <p className='h3-medium mb-2'>Subscribe</p>
           <span className='mb-1'>Get 10% off your first order</span>
 
-          <Input placeholder='Enter your email' className='max-w-xs'/>
+          <Input
+            placeholder='Enter your email'
+            className='max-w-xs'
+          />
         </div>
 
         <nav className='flex-1 relative z-50'>
           <ul className='flex gap-20'>
             {FooterNavLinks.map(({ label, links }) => (
               <li key={label}>
-                <h3 className='h3-medium mb-5'>{label}</h3>
+                <h3 className='h3-medium mb-5 whitespace-nowrap'>{label}</h3>
                 <div className='flex flex-col gap-3'>
                   {links.map((link) => (
                     <NextLink {...link}>{link.label}</NextLink>
@@ -40,16 +44,14 @@ export function Footer() {
         </nav>
       </div>
 
-      <div className='footer-bottom'>
-        <div className='container'>
-          <p className='copyright-text'>
-            &copy; 2022{' '}
-            <a href='#' className='copyright-link'>
-              wakilabdulazeez
-            </a>{' '}
-            All Rights Reserved.
-          </p>
-        </div>
+      <div className='py-[20px] text-center'>
+        <p>
+          &copy; 2022{' '}
+          <a href='#' className='inline-block'>
+            wakilabdulazeez
+          </a>{' '}
+          All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
